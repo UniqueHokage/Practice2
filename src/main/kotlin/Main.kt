8bfe34d867e9dcbe7f7d4918db19087af31b5d87
UniqueHokage <123321855+UniqueHokage@users.noreply.github.com>
2023-09-09 10:07:17 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,11 +3,13 @@ fun main(args: Array<String>) {
     val age1 = 42;
     val age2 = 21;
 
-    //2-3 задание
-    val age11 = age1.toDouble()
-    val age22 = age2.toDouble()
+    //2 задание
     val avg1 = (age11 + age22) / 2;
     println("2-3. Среднее арифм. $avg1")
+
+    //3 задание
+    val age11 = age1.toDouble()
+    val age22 = age2.toDouble()
 
     //4 задание
     val firstName = "Roman"
@@ -21,9 +23,13 @@ fun main(args: Array<String>) {
     val myDetails = "Привет, меня зовут $fullName."
     println("6. $myDetails")
 
-    //7-9 задание
+    //7 задание
     val triple = Triple (7,9,2023)
+    
+    //8 задание
     val (day,month,year) = triple;
+    
+    //9 задание
     val (month1, _ , year1) = triple;
 
     //10 задание
